709790cc5e6f43c312232f924df958bc8ed86a81
jst-r <jstr271@gmail.com>
2024-02-29 10:29:53 +0100

    remove the interpreter

--- src/interpreter/runtime/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod callable;
-pub mod class;
-pub mod function;
-pub mod result;
-pub mod value;
--- src/visitor.rs
+++
@@ -1,6 +0,0 @@
-pub trait AcceptMut<V, R>
-where
-    Self: Sized,
-{
-    fn accept(&self, visitor: &mut V) -> R;
-}
